0d1f21dcc63bbea60c34471161f0b60a6ad0ac6f
jatinder34 <jatinderkumar0550@gmail.com>
2021-07-27 18:21:48 +0530

    first code

---
+++ src/side.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+
+
+
+function First() {
+    return (
+        <div class="first">
+            <h1>Text</h1>
+        </div>
+    )
+}
+
+
+
+
+export default App;
